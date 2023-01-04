e6ef3010429a089eedfc8eb36be30a15a829631d
Tom van Eijk <tom.vaneijk@bain.com>
2023-01-04 14:01:41 +0100

    Colours added LaTex table

--- python/data_as_a_product_codeablemodels/generators/generate_latex_files.py
+++ python/data_as_a_product_codeablemodels/generators/generate_latex_files.py
@@ -18,18 +18,18 @@ def render_table_header_line(headers):
 
 def get_color_coding_command(percentage):
     if percentage < 0.05:
-        return "\\cll"
+        return "\\cellcolor{emerald_shape_1}"
     if percentage < 0.10:
-        return "\\cl"
+        return "\\cellcolor{emerald_shape_2}"
     if percentage < 0.20:
-        return "\\cml"
+        return "\\cellcolor{emerald_shape_3}"
     if percentage < 0.35:
-        return "\\cm"
+        return "\\cellcolor{emerald_shape_4}"
     if percentage < 0.5:
-        return "\\cmh"
+        return "\\cellcolor{emerald_shape_5}"
     if percentage < 0.7:
-        return "\\ch"
-    return "\\chh"
+        return "\\cellcolor{emerald_shape_6}"
+    return "\\cellcolor{emerald_shape_7}"
 
 
 def get_dict(data):
@@ -132,7 +132,7 @@ class LatexTableRenderer(object):
 
     def generate_overview_table(self, decisions):
         total_number_of_sources = len(source.classes)
-        text = "\\begin{tabular}{|p{0.12\linewidth}|p{0.005\linewidth}|p{0.3\linewidth}|p{0.15\linewidth}|p{0.31\linewidth}|}\n"
+        text = "\\begin{tabular}{|p{0.12\linewidth}|p{0.015\linewidth}|p{0.3\linewidth}|p{0.15\linewidth}|p{0.31\linewidth}|}\n"
         text += "\\hline\n"
         text += render_table_header_line(["Design Decision", "\#", "Solution", "Evidences", "Forces"])
         text += "\\hline\n"
@@ -153,10 +153,10 @@ class LatexTableRenderer(object):
             force_list =[]
             for i, option in enumerate(decision.get_linked(association=decision_solution_relation)):
                 current = current + 1
-                if number_of_solutions is not 1 and current is number_of_solutions - 1:
+                if number_of_solutions != 1 and current is number_of_solutions - 1:
                     text += " & \\multirow{-" + str(number_of_solutions) + "}{\linewidth}{ " + color_command_decision + "{" + str(
                         total_number_of_evidences) + "}} &"
-                elif current is not 0:
+                elif current != 0:
                     text += " & " + color_command_decision + "{} & "
                 
                 text += str(i + 1) + "." + str(option.name) + "&"
@@ -169,11 +169,11 @@ class LatexTableRenderer(object):
                 for _force in _forces:
                     for key, value in get_dict(force.all_classes).items():
                         if _force[2] == value.name and _forces.index(_force) < len(_forces)-1:
-                            if _forces.index(_force) is not 0:
+                            if _forces.index(_force) != 0:
                                 text += " "
                             text += key + "(" + parse_double_minus_sign(_force[1]) + "),"
                         elif _force[2] == value.name and _forces.index(_force) is len(_forces)-1:
-                            if _forces.index(_force) is not 0:
+                            if _forces.index(_force) != 0:
                                 text += " "
                             text += key + "(" + parse_double_minus_sign(_force[1]) + ")"
 
